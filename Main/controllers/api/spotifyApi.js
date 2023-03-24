aa561ae983a1512daf86aef7ae63f716d615f93c
Jeremy McCarter <jeremymccarter27@gmail.com>
2023-03-23 20:31:59 -0700

    Setup spotifyApi call function, seems to be working

---
+++ Main/controllers/api/spotifyApi.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const dotenv = require("dotenv")
+dotenv.config()
+
+//axios.request(options).then(function (response) {
+  //console.log(response.data);
+//}).catch(function (error) {
+  //console.error(error);
+//});
+
+async function spotifyApi() {
+  const options = {
+    method: 'GET',
+    url: 'https://spotify23.p.rapidapi.com/artists/',
+    params: { ids: '2w9zwq3AktTeYYMuhMjju8' },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  };
+  try {
+    const response = await axios.request(options)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+
+
+}
+//BE SURE TO KEEP THINGS ASYNC
+
+
+module.exports=spotifyApi
