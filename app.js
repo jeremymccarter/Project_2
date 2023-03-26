aafe5af49299a9b9b19fc3fff2c465d550f802f3
Tri Chau <trichauphuoc@gmail.com>
2023-03-26 00:47:11 -0700

    first layout of handlebars

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('index');
+});
+
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`);
+});
